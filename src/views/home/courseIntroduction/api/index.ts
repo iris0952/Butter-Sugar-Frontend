1486ae60e0daf3bc3213512f02e39b60aa44c205
Cami Wang <iris0952@gmail.com>
2025-07-11 15:13:05 +0800

    fix: 修改課程章節 api

--- src/views/home/courseIntroduction/api/index.ts
+++ src/views/home/courseIntroduction/api/index.ts
@@ -2,7 +2,12 @@ import axios from '@/api/axios';
 import type { SectionResponse } from './type';
 import type { AxiosResponse } from 'axios';
 
-// 獲取課程章節
+// 獲取單一課程資料
 export const getCourseSections = async (
   courseId: string
-): Promise<AxiosResponse<SectionResponse>> => axios.get(`/api/v1/course/${courseId}`);
+): Promise<AxiosResponse<SectionResponse>> => axios.get(`/api/v1/course/${courseId}/section`);
+
+// 獲取課程章節
+// export const getOneCourseSections = async (
+//   courseId: string
+// ): Promise<AxiosResponse<SectionResponse>> => axios.get(`/api/v1/course/${courseId}/section`);
